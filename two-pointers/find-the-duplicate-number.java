7104b5c682a86eea9788776df09bfd7943f3b991
Derek Sun <77709066+DerekSun-xin@users.noreply.github.com>
2025-07-22 12:00:36 -0400

    LC-find-the-duplicate-number [Runtime: 4 ms; Memory: 58.5 MB]

--- two-pointers/find-the-duplicate-number.java
+++ two-pointers/find-the-duplicate-number.java
@@ -1,9 +1,9 @@
 class Solution {
     public int findDuplicate(int[] nums) {
         while(nums[0] != nums[nums[0]]){
-            int temp = nums[0];
-            nums[0] = nums[nums[0]];
-            nums[temp] = temp; 
+           int temp = nums[nums[0]];
+           nums[nums[0]] = nums[0];
+           nums[0] = temp; 
         }
         return nums[0]; 
     }
