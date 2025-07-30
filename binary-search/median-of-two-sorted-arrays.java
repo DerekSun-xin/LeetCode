c06ce09d931a017957dacbe37417ad15b7e3d402
Derek Sun <77709066+DerekSun-xin@users.noreply.github.com>
2025-07-30 14:16:38 -0400

    LC-median-of-two-sorted-arrays [Runtime: 1 ms; Memory: 46.4 MB]

--- binary-search/median-of-two-sorted-arrays.java
+++ binary-search/median-of-two-sorted-arrays.java
@@ -1,32 +1,57 @@
 class Solution {
-    private int p1, p2 = 0; 
-
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        // Merge Sort: 
-        int length = nums1.length + nums2.length; 
-        if (length % 2 ==0){
-            for(int i = 0; i < length/2 - 1; i++){
-                int tmp = getMin(nums1, nums2); 
-            }
-            return (getMin(nums1, nums2) + getMin(nums1, nums2)) / 2.0; 
+        int m = nums1.length;
+        int n = nums2.length; 
+        int k = (m + n) / 2; 
+        if ((m+n) % 2 == 1){
+            return solve(nums1, nums2, k, 0, m - 1, 0, n -1); 
         }else{
-            for(int i = 0; i < length/2; i++){
-                int tmp = getMin(nums1, nums2); 
-            }
-            return getMin(nums1, nums2); 
+            return (solve(nums1, nums2, k, 0, m-1, 0, n-1) + solve(nums1,nums2,k - 1, 0, m-1, 0, n-1)) / 2.0; 
         }
-        
     }
 
-    // Get the smaller value between nums1[p1] and nums2[p2] and move the pointer forwards 
-    private int getMin(int[] nums1, int[] nums2){
-        if (p1 < nums1.length && p2 < nums2.length){
-            return nums1[p1] <= nums2[p2] ? nums1[p1++]: nums2[p2++]; 
-        }else if(p1 < nums1.length){
-            return nums1[p1++]; 
-        }else if(p2 < nums2.length){
-            return nums2[p2++]; 
+    public int solve(
+        int[] A, 
+        int[] B, 
+        int k, 
+        int aStart,
+        int aEnd, 
+        int bStart, 
+        int bEnd
+    ){
+        if (aStart > aEnd){
+            return B[k - aStart];
         }
-        return -1; 
+        if (bStart > bEnd){
+            return A[k - bStart]; 
+        }
+        // Get the middle index of two ranges: 
+        int aIndex = (aStart+aEnd)/2;
+        int bIndex = (bStart+bEnd)/2;
+        // Get the middle value of two ranges:
+        int aMidVal = A[aIndex];
+        int bMidVal = B[bIndex]; 
+        // Cut one range by half each time based on condition: 
+        if (k > aIndex + bIndex){
+            // Remove the left part of smaller range
+            if (aMidVal > bMidVal){
+                bStart = bIndex + 1; 
+                return solve(A,B,k, aStart, aEnd, bStart, bEnd); 
+            }else{
+                aStart = aIndex + 1;
+                return solve(A,B,k, aStart, aEnd, bStart, bEnd); 
+            }
+        }else{
+            // Remove the right part of the larger range
+            if (aMidVal > bMidVal){
+                aEnd = aIndex - 1;
+                return solve(A,B,k, aStart, aEnd, bStart, bEnd); 
+
+            }else{
+                bEnd = bIndex - 1;
+                return solve(A,B,k, aStart, aEnd, bStart, bEnd); 
+            }
+        }
+        
     }
 }
