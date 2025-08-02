d46726e7b50167e949ecb5f123b4d2796381dcdc
Derek Sun <77709066+DerekSun-xin@users.noreply.github.com>
2025-08-02 12:04:55 -0400

    LC-string-to-integer-atoi [Runtime: N/A; Memory: N/A]

---
+++ string/string-to-integer-atoi.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int myAtoi(String s) {
+        int sign = 1;
+        int curInt = 0; 
+        boolean isLeadZero = true; 
+        for(int i = 0; i < s.length(); i++){
+            char curChar = s.charAt(i); 
+            int digit = curChar - '0'; 
+            if (curChar == ' '){
+                continue;
+            }
+            if (curChar == '-'){
+                sign = -1;
+                continue; 
+            }
+            if (!Character.isDigit(curChar)){
+                break; 
+            }
+            if(digit == 0 && isLeadZero){
+                isLeadZero = false; 
+                continue; 
+            }
+            if (sign == 1){
+                if (curInt > Integer.MAX_VALUE/10){
+                    return Integer.MAX_VALUE;
+                }else if (curInt == Integer.MAX_VALUE/10 && digit > Integer.MAX_VALUE % 10){
+                    return Integer.MAX_VALUE; 
+                }
+                curInt = curInt * 10 + digit; 
+            }else{
+                if(curInt > 0){
+                    curInt = sign * curInt; 
+                }
+                if (curInt < Integer.MIN_VALUE/10){
+                    return Integer.MIN_VALUE;
+                }else if(curInt == Integer.MIN_VALUE/10 && digit < Integer.MIN_VALUE % 10){
+                    return Integer.MIN_VALUE;
+                }
+                curInt = curInt * 10 - digit; 
+            }
+        }
+        return curInt; 
+    }
+}
