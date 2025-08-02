0f01c7d3768c9620fa35f786739f2c275e2bb726
Derek Sun <77709066+DerekSun-xin@users.noreply.github.com>
2025-08-02 17:08:12 -0400

    LC-string-to-integer-atoi [Runtime: N/A; Memory: N/A]

--- string/string-to-integer-atoi.java
+++ string/string-to-integer-atoi.java
@@ -1,34 +1,28 @@
 class Solution {
     public int myAtoi(String s) {
-        // 1. Initialize all variables 
-        int index = 0; 
-        int sign = 1; 
+        // Initialize all variables
         int result = 0; 
-        int n = s.length(); 
-        if (n == 0){
-            return 0; 
+        int index = 0;
+        int n = s.length();
+        int sign = 1;
+        // Deal with whitespaces 
+        while(index < n && s.charAt(index) == ' '){
+            index++;
         }
-
-        // 2. Filter out all whitespaces
-        while (index < n && s.charAt(index) == ' '){
-            index++; 
+        // Deal with sign
+        if (index < n && (s.charAt(index) == '+') || (s.charAt(index) == '-')){
+            sign = s.charAt(index) == '+'? 1: -1;
+            index++;
         }
-
-        // 3. Determine the sign
-        if(index < n & (s.charAt(index) == '+' || s.charAt(index) == '-')){
-            sign = s.charAt(index) == '+' ? 1: -1; 
-            index++; 
-        }
-
-        // 4. Deal with digit
+        // Deal with digits 
         while(index < n && Character.isDigit(s.charAt(index))){
-            int digit = s.charAt(index) - '0'; 
-            if (result > Integer.MAX_VALUE/10 || (result == Integer.MAX_VALUE/10 && digit > Integer.MAX_VALUE % 10)){
-                return sign == 1?Integer.MAX_VALUE: Integer.MIN_VALUE; 
+            int digit = s.charAt(index) - '0';
+            if (result > Integer.MAX_VALUE || (result == Integer.MAX_VALUE && digit > Integer.MAX_VALUE)){
+                return (sign == 1) ? Integer.MAX_VALUE: Integer.MIN_VALUE;
             }
-            result = result*10 + digit; 
-            index++; 
+            result = result * 10 + digit;
+            index++;
         }
-        return sign * result; 
+        return sign * result;
     }
 }
