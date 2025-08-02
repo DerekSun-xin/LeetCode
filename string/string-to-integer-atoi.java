fe121e28b727e85541968f8da97b955d8a3cb7f3
Derek Sun <77709066+DerekSun-xin@users.noreply.github.com>
2025-08-02 13:17:24 -0400

    LC-string-to-integer-atoi [Runtime: N/A; Memory: N/A]

--- string/string-to-integer-atoi.java
+++ string/string-to-integer-atoi.java
@@ -1,56 +1,31 @@
 class Solution {
     public int myAtoi(String s) {
-        int sign = 1;
-        int curInt = 0; 
-        boolean isLeadZero = true; 
-        boolean isLeadSign = true; 
-        for(int i = 0; i < s.length(); i++){
-            char curChar = s.charAt(i); 
-            int digit = curChar - '0'; 
-            if (curChar == ' '){
-                continue;
-            }
-            if (curChar == '-' && isLeadSign){
-                isLeadSign = false; 
-                sign = -1;
-                continue; 
-            }else if(curChar == '-'){
-                break; 
-            }
-            if (curChar == '+' && isLeadSign){
-                isLeadSign = false; 
-                continue; 
-            }else if(curChar == '+'){
-                break; 
-            }
-            if (!Character.isDigit(curChar)){
-                break; 
-            }
-            if(digit == 0 && isLeadZero){
-                isLeadZero = false; 
-                isLeadSign = false;
-                continue; 
-            }
-            isLeadSign = false;
-            if (sign == 1){
-                if (curInt > Integer.MAX_VALUE/10){
-                    return Integer.MAX_VALUE;
-                }else if (curInt == Integer.MAX_VALUE/10 && digit > Integer.MAX_VALUE % 10){
-                    return Integer.MAX_VALUE; 
-                }
-                curInt = curInt * 10 + digit; 
-            }else{
-                if(curInt > 0){
-                    curInt = sign * curInt; 
-                }
-                if (curInt < Integer.MIN_VALUE/10){
-                    return Integer.MIN_VALUE;
-                }else if(curInt == Integer.MIN_VALUE/10 && digit < Integer.MIN_VALUE % 10){
-                    return Integer.MIN_VALUE;
-                }
-                curInt = curInt * 10 - digit; 
-            }
+        // 1. Initialize all variables 
+        int index = 0; 
+        int sign = 1; 
+        int result = 0; 
+        int n = s.length(); 
+
+        // 2. Filter out all whitespaces
+        while (index < n && s.charAt(index) == ' '){
+            index++; 
         }
-        return curInt; 
+
+        // 3. Determine the sign
+        if(index < n & (s.charAt(index) == '+' || s.charAt(index) == '-')){
+            sign = s.charAt(index) == '+' ? 1:-1; 
+            index++; 
+        }
+
+        // 4. Deal with digit
+        while(index < n && Character.isDigit(s.charAt(index))){
+            int digit = s.charAt(index) - '0'; 
+            if (result > Integer.MAX_VALUE/10 || (result == Integer.MAX_VALUE/10 && digit > Integer.MAX_VALUE % 10)){
+                return Integer.MAX_VALUE; 
+            }
+            result = result*10 + digit; 
+            index++; 
+        }
+        return sign * result; 
     }
 }
