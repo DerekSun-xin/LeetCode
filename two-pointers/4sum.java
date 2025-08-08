da36b9ad95efe7d6eab087fc008ead012d68bcb8
Derek Sun <77709066+DerekSun-xin@users.noreply.github.com>
2025-08-07 21:24:07 -0400

    LC-4sum [Runtime: N/A; Memory: N/A]

--- two-pointers/4sum.java
+++ two-pointers/4sum.java
@@ -1,42 +1,58 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
+        Arrays.sort(nums); 
+        return kSum(nums, 0, 4, target); 
+    }
+    
+    public List<List<Integer>> kSum(int[] nums, int start, int k, int target){
         List<List<Integer>> result = new ArrayList<>();
-        Arrays.sort(nums);
-        int n = nums.length;
-
-        for (int i = 0; i < n-3; i++){
-            if(i!=0){
-                if(nums[i] == nums[i-1]){
-                    continue;
-                }
+        int n = nums.length; 
+        if (start >= n){
+            return result; 
+        }else if(nums[start] * k > target){
+            return result;
+        }else if(nums[n-1]*k < target){
+            return result; 
+        }
+        if (k == 2){
+            return twoSum(nums,target,start); 
+        }
+        for(int i = start; i < n; i++){
+            if (i>start && nums[i]==nums[i-1]){
+                continue; 
             }
-            for (int j = i+1; j < n-2; j++){
-                if(j!=1){
-                    if(nums[j] == nums[j-1]){
-                        continue; 
-                    }
-                }
-                int left = j+1;
-                int right = n-1;
-                while(left < right){
-                    long sum = (long)nums[i]+nums[j]+nums[left]+nums[right];
-                    if (sum > target){
-                        right--;
-                        while(right > 0 && nums[right] == nums[right+1]){
-                            right--;
-                        }
-                    }else if(sum < target){
-                        left++;
-                        while(left < n && nums[left]==nums[left-1]){
-                            left++;
-                        }
-                    }else{
-                        result.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
-                        break; 
-                    }
-                }
+            List<List<Integer>> subList = kSum(nums, i+1, k-1, target - nums[i]); 
+            for(List<Integer> tmp:subList){
+                List<Integer> newList = new ArrayList<>(tmp); 
+                newList.add(nums[i]); 
+                result.add(newList); 
             }
         }
-        return result;
+        return result; 
+    }
+    
+    public List<List<Integer>> twoSum(int[] nums, int target, int start){
+        List<List<Integer>> res = new ArrayList<>(); 
+        int left = start;
+        int right = nums.length-1; 
+        while (left < right){
+            int sum = nums[left] + nums[right]; 
+            if (sum > target){
+                right--;
+            }else if (sum < target){
+                left++;  
+            }else{
+                res.add(Arrays.asList(nums[left], nums[right]));
+                right--;
+                left++; 
+                while(left < right && nums[right] == nums[right+1]){
+                    right--; 
+                }
+                while(left < right && nums[left] == nums[left-1]){
+                    left++; 
+                }     
+            }
+        }
+        return res; 
     }
 }
