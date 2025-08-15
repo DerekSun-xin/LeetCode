3447a3d2745a3927ce7f67576e85410e2de16b84
Derek Sun <77709066+DerekSun-xin@users.noreply.github.com>
2025-08-15 00:46:20 -0400

    LC-regular-expression-matching [Runtime: N/A; Memory: N/A]

--- dynamic-programming/regular-expression-matching.java
+++ dynamic-programming/regular-expression-matching.java
@@ -5,22 +5,22 @@ class Solution {
         }
 
         boolean[][] dp = new boolean[s.length()+1][p.length()+1];
-
-        for (int j = 1; j < p.length(); j++){
+        dp[0][0] = true;
+        for (int j = 1; j <= p.length(); j++){
             if (p.charAt(j-1) == '*'){
                 dp[0][j]=dp[0][j-2];
             }
         }
 
-        for(int i = 1; i < s.length(); i++){
-            for(int j = 1; j < p.length(); j++){
+        for(int i = 1; i <= s.length(); i++){
+            for(int j = 1; j <= p.length(); j++){
                 char sChar = s.charAt(i-1);
                 char pChar = p.charAt(j-1);
                 if (pChar == '.' || pChar == sChar){
                     dp[i][j] = dp[i-1][j-1];
                 }else if (pChar == '*'){
                     dp[i][j]=dp[i][j-2];
-                    char prevPChar = p[j-2];
+                    char prevPChar = p.charAt(j-2);
                     if (sChar == prevPChar || prevPChar == '.' ){
                         dp[i][j] = dp[i][j] || dp[i-1][j-1];
                     }
@@ -29,6 +29,7 @@ class Solution {
                 }
             }
         }
+        return dp[s.length()][p.length()];
 
     }
 }
