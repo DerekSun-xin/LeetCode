cd07dd2caa6d6ad6f41d287e46d8868c8651cbfe
Derek Sun <77709066+DerekSun-xin@users.noreply.github.com>
2025-08-30 17:17:34 -0400

    LC-reverse-nodes-in-k-group [Runtime: 0 ms; Memory: 44.3 MB]

--- linked-list/reverse-nodes-in-k-group.java
+++ linked-list/reverse-nodes-in-k-group.java
@@ -10,44 +10,61 @@
  */
 class Solution {
     public ListNode reverseKGroup(ListNode head, int k) {
-        // Turn the ListNode into a list
-        List<Integer> ls = new ArrayList<>();
-        ListNode tmp = head;
-        while(tmp != null){
-            ls.add(tmp.val);
-            tmp = tmp.next; 
+        // Handle edge case where head is null or k = 1
+        if (head == null || k == 1){
+            return head;
         }
-        // Handle edge case where list size <= k
-        if (ls.size() < k){
-            return head; 
-        }
-        // Iterate the list to reverse the list k at a time
-        for (int i = 1; i <= ls.size(); i++){
-            if (i % k == 0){
-                int left = i - k; 
-                int right = i - 1; 
-                swap(ls, left, right); 
-            }
-        }
-        // Turn the reversed list into a ListNode and return
+
+        // Create a dummyHead
         ListNode dummyHead = new ListNode();
-        tmp = dummyHead;
-        for (int val :ls){
-            ListNode cur = new ListNode(val);
-            tmp.next = cur;
-            tmp = tmp.next; 
+        dummyHead.next = head;
+
+        // Initialize groupPrev
+        ListNode groupPrev = dummyHead; 
+
+        while (true){
+            // Get the last node of the current group 
+            ListNode kthNode = getKthNode(groupPrev, k); 
+            if (kthNode == null){
+                break; 
+            }
+
+            ListNode groupStart = groupPrev.next; // 1st node of current group 
+            ListNode groupNext = kthNode.next; // 1st Node of next group
+            
+            // Reverse the current group
+            kthNode.next = null; // Disconnect the current group and the rest of the list 
+            ListNode reversedHead = reverseLinkedList(groupStart); 
+
+            // Reconnect the reversed group
+            groupPrev.next = reversedHead; // Link previous group to new head
+            groupStart.next = groupNext; // Link next group to new tail 
+            
+            // Update the groupPrev to the new tail
+            groupPrev = groupStart; 
         }
-        return dummyHead.next;
+
+        return dummyHead.next; 
+    }
+    private ListNode getKthNode(ListNode head, int k){
+        ListNode tmp = head;
+        int count = 0;
+        while(count < k && tmp != null){
+            tmp = tmp.next;
+            count++;  
+        }
+        return tmp;  
     }
 
-    private void swap(List<Integer> ls, int left, int right){
-        int swapNum = (right - left) / 2 + 1; 
-        while(left < right){
-            int tmp = ls.get(left);
-            ls.set(left, ls.get(right));
-            ls.set(right, tmp); 
-            left++;
-            right--; 
+    private ListNode reverseLinkedList(ListNode head){
+        ListNode prev = null;
+        ListNode cur = head;
+        while(cur != null){
+            ListNode nextTmp = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = nextTmp; 
         }
+        return prev;
     }
 }
