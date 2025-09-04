4ac691118cbae17e02fc6d8bf2034de6b140cd70
Derek Sun <77709066+DerekSun-xin@users.noreply.github.com>
2025-09-04 14:56:09 -0400

    LC-divide-two-integers [Runtime: N/A; Memory: N/A]

---
+++ bit-manipulation/divide-two-integers.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int divide(int dividend, int divisor) {
+        if (dividend == 0){
+            return 0; 
+        }
+        int quotient = 0; 
+        if (dividend > 0 && divisor > 0){
+            while(dividend > 0){
+                dividend-=divisor; 
+                quotient++;
+            }
+            return quotient-1; 
+        }else if (dividend < 0 && divisor < 0){
+            while (dividend < 0){
+                dividend-=divisor; 
+                quotient++; 
+            }
+            return quotient-1; 
+        }else if (dividend < 0 && divisor > 0){
+            while(dividend < 0){
+                dividend += divisor; 
+                quotient++; 
+            }
+            return -(quotient-1); 
+        }else if (dividend > 0 && divisor < 0){
+            while(dividend > 0){
+                dividend += divisor; 
+                quotient++; 
+            }
+            return -(quotient-1); 
+        }else{
+            return 0; 
+        }
+    }
+}
