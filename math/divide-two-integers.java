da9d3265557692d563d8213617bd72ce0dc06043
Derek Sun <77709066+DerekSun-xin@users.noreply.github.com>
2025-09-05 13:16:10 -0400

    LC-divide-two-integers [Runtime: 1 ms; Memory: 41.4 MB]

--- math/divide-two-integers.java
+++ math/divide-two-integers.java
@@ -1,27 +1,31 @@
 class Solution {
     public int divide(int dividend, int divisor) {
+        // Handle Edge Case
         if (dividend == Integer.MIN_VALUE && divisor == -1){
-            return Integer.MAX_VALUE; 
+            return Integer.MAX_VALUE;
         }
+
         // Determine the sign of the quotient
-        // Return sign * quotient in the end
+        // Return sign * quotient as our final return
         int sign = 1;
         if ((dividend > 0 && divisor < 0) || (dividend < 0 && divisor > 0)){
-            sign = -1; 
+            sign = -1;
         }
-        int quotient = 0;
-        long absDividend = Math.abs(dividend); 
-        long absDivisor = Math.abs(divisor); 
-        long remainder = absDividend; 
 
+        int quotient = 0;
+        long absDividend = Math.abs((long)dividend);
+        long absDivisor = Math.abs((long)divisor);
+        long remainder = absDividend;
+        
         while (remainder >= absDivisor){
             int shift = 0;
-            while(remainder > absDivisor * (int) Math.pow(2,shift+1)){
-                shift++; 
+            while (remainder >= absDivisor << (shift + 1)){ // pow returns double type; Need to convert to int
+                shift++;
             }
-            quotient += (int) Math.pow(2,shift); 
-            remainder -= absDivisor * (int) Math.pow(2,shift); 
+            quotient += 1 << shift;
+            remainder -= absDivisor << shift;
         }
-        return sign * quotient; 
+
+        return sign * quotient;
     }
 }
