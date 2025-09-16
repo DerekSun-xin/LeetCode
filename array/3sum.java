dab8ad7a102d2c1cbb830824be3de58968b3090a
Derek Sun <77709066+DerekSun-xin@users.noreply.github.com>
2025-09-16 15:01:02 -0400

    LC-3sum [Runtime: N/A; Memory: N/A]

---
+++ array/3sum.java
@@ -0,0 +1,29 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> ret = new ArrayList<>(); 
+
+        // Sort nums O(nlogn)
+        Arrays.sort(nums);  
+
+        // For loop O(n^2)
+        for (int i = 0; i < nums.length; i++){
+            if (i != 0 && nums[i] == nums[i-1]){
+                continue; 
+            }
+            int start = i+1;
+            int end = nums.length - 1; 
+            while (start < end){
+                int sum = nums[i] + nums[start] + nums[end]; 
+                if (sum > 0){
+                    end--;
+                }else if (sum < 0){
+                    start++;
+                }else{
+                    ret.add(new ArrayList<>(List.of(nums[i], nums[start], nums[end]))); 
+                    start
+                }
+            }
+        }
+        return ret; 
+    }
+}
