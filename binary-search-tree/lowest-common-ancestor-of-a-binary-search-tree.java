913541342c5828a74f4f7b19db31fe65554d8f36
Derek Sun <77709066+DerekSun-xin@users.noreply.github.com>
2025-11-03 10:25:08 -0500

    LC-lowest-common-ancestor-of-a-binary-search-tree [Runtime: 6 ms; Memory: 47.8 MB]

--- binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.java
+++ binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.java
@@ -10,15 +10,16 @@
 
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if (root == null){
-            return null; 
+        // This is a BST. 
+        while(root != null){
+            if (p.val < root.val && q.val < root.val){
+                root = root.left; 
+            }else if (p.val > root.val && q.val > root.val){
+                root = root.right;
+            }else{
+                return root; 
+            }
         }
-        if (p.val > root.val && q.val > root.val){
-            return lowestCommonAncestor(root.right, p, q); 
-        }
-        if (p.val < root.val && q.val < root.val){
-            return lowestCommonAncestor(root.left, p, q); 
-        }
-        return root; 
+        return null; 
     }
 }
