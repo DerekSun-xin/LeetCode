3a152ab9816b6ba53c5b4ad1a114930c28cf9eab
Derek Sun <77709066+DerekSun-xin@users.noreply.github.com>
2025-11-17 14:20:18 -0500

    LC-kth-largest-element-in-a-stream [Runtime: 23 ms; Memory: 52.4 MB]

--- binary-tree/kth-largest-element-in-a-stream.java
+++ binary-tree/kth-largest-element-in-a-stream.java
@@ -1,8 +1,10 @@
 class KthLargest {
     private PriorityQueue<Integer> pq; 
+    private int k; 
 
     public KthLargest(int k, int[] nums) {
         this.pq = new PriorityQueue<>(k); 
+        this.k = k; 
         for (int i = 0; i < nums.length; i++){
             pq.add(nums[i]); 
             if (pq.size() > k){
@@ -12,13 +14,13 @@ class KthLargest {
     }
     
     public int add(int val) {
-        if (pq.isEmpty()){
-            pq.add(val); 
-        }
-        if (val > pq.peek()){
+        if (pq.isEmpty() || pq.size() < k){
+            pq.add(val);
+        }else if(val > pq.peek()){
             pq.poll(); 
             pq.add(val);
         }
+        
         return pq.peek(); 
     }
 }
