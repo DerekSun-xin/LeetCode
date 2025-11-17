82de27ff5860891f02f1f61b7e8fcac78887e323
Derek Sun <77709066+DerekSun-xin@users.noreply.github.com>
2025-11-17 12:50:33 -0500

    LC-kth-largest-element-in-a-stream [Runtime: N/A; Memory: N/A]

--- heap-priority-queue/kth-largest-element-in-a-stream.java
+++ heap-priority-queue/kth-largest-element-in-a-stream.java
@@ -2,21 +2,29 @@ class KthLargest {
     private PriorityQueue<Integer> pq; 
 
     public KthLargest(int k, int[] nums) {
-        Arrays.sort(nums); 
+        nums = Arrays.stream(nums)
+                .boxed()
+                .sorted(Collections.reverseOrder())
+                .mapToInt(Integer::intValue)
+                .toArray(); 
+
         this.pq = new PriorityQueue<>(k); 
-        for (int i = nums.length-1; i > nums.length-1-k; i--){
-            pq.add(nums[i]); 
+        if (k < nums.length){
+            for (int i = 0; i < k; i++){
+                pq.add(nums[i]); 
+            }
         }
     }
     
     public int add(int val) {
+        if (pq.isEmpty()){
+            pq.add(val); 
+        }
         if (val > pq.peek()){
             pq.poll(); 
             pq.add(val);
-            return pq.peek(); 
-        }else{
-            return pq.peek(); 
         }
+        return pq.peek(); 
     }
 }
 
