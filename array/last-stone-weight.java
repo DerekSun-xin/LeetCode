1b5d7c481453f07926c60e7a79f6f73a872605fc
Derek Sun <77709066+DerekSun-xin@users.noreply.github.com>
2025-11-18 11:34:29 -0500

    LC-last-stone-weight [Runtime: N/A; Memory: N/A]

--- array/last-stone-weight.java
+++ array/last-stone-weight.java
@@ -1,14 +1,15 @@
 class Solution {
     public int lastStoneWeight(int[] stones) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-        for(int stone: stones){
+        // Use max-heap 
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder()); 
+        for (int stone: stones){
             pq.add(stone); 
         }
         while (pq.size() != 1){
-            int largest = pq.poll();
-            int secondLargest = pq.poll();
-            int diff = largest - secondLargest; 
-            if (diff >= 0){
+            int heaviest = pq.poll();
+            int secondHeaviest = pq.poll();
+            int diff = heaviest - secondHeaviest;
+            if (diff > 0){
                 pq.add(diff); 
             }
         }
